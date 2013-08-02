31320db37e01c7f90c1ac1eee91f373a70925e1f
Oleksandr Rebrov <webmaster@webplaneta.com.ua>
2013-08-02 20:29:13 +0300

    Merge image tools with 1.5.5.1

--- catalog/model/tool/image.php
+++ catalog/model/tool/image.php
@@ -1,15 +1,27 @@
 <?php
 class ModelToolImage extends Model {
-	public function resize($filename, $width, $height) {
+	/**
+	*	
+	*	@param filename string
+	*	@param width 
+	*	@param height
+	*	@param type char [default, w, h]
+	*				default = scale with white space, 
+	*				w = fill according to width, 
+	*				h = fill according to height
+	*	
+	*/
+	public function resize($filename, $width, $height, $type = "") {
 		if (!file_exists(DIR_IMAGE . $filename) || !is_file(DIR_IMAGE . $filename)) {
 			return;
 		} 
 		
 		$info = pathinfo($filename);
+		
 		$extension = $info['extension'];
 		
 		$old_image = $filename;
-		$new_image = 'cache/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . $width . 'x' . $height . '.' . $extension;
+		$new_image = 'cache/' . utf8_substr($filename, 0, utf8_strrpos($filename, '.')) . '-' . $width . 'x' . $height . $type .'.' . $extension;
 		
 		if (!file_exists(DIR_IMAGE . $new_image) || (filemtime(DIR_IMAGE . $old_image) > filemtime(DIR_IMAGE . $new_image))) {
 			$path = '';
@@ -28,7 +40,7 @@ class ModelToolImage extends Model {
 
 			if ($width_orig != $width || $height_orig != $height) {
 				$image = new Image(DIR_IMAGE . $old_image);
-				$image->resize($width, $height);
+				$image->resize($width, $height, $type);
 				$image->save(DIR_IMAGE . $new_image);
 			} else {
 				copy(DIR_IMAGE . $old_image, DIR_IMAGE . $new_image);
@@ -36,9 +48,9 @@ class ModelToolImage extends Model {
 		}
 		
 		if (isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'))) {
-			return HTTPS_IMAGE . $new_image;
+			return $this->config->get('config_ssl') . 'image/' . $new_image;
 		} else {
-			return HTTP_IMAGE . $new_image;
+			return $this->config->get('config_url') . 'image/' . $new_image;
 		}	
 	}
 }
