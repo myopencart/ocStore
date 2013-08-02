1715094426d8d7524ce9eb2acb3f134b13278ba6
Oleksandr Rebrov <webmaster@webplaneta.com.ua>
2013-08-02 19:49:00 +0300

    Merge catalog\controller\module with opencart 1.5.5.1

--- catalog/controller/module/language.php
+++ catalog/controller/module/language.php
@@ -1,19 +1,24 @@
 <?php  
 class ControllerModuleLanguage extends Controller {
-	public function index() {
+	protected function index() {
     	if (isset($this->request->post['language_code'])) {
 			$this->session->data['language'] = $this->request->post['language_code'];
+
 
 			if (isset($this->request->post['redirect'])) {
 				$this->redirect($this->request->post['redirect']);
 			} else {
 				$this->redirect($this->url->link('common/home'));
 			}
+
+
     	}
 
 		$this->language->load('module/language');
 
+
 		$this->data['text_language'] = $this->language->get('text_language');
+
 
 		if (isset($this->request->server['HTTPS']) && (($this->request->server['HTTPS'] == 'on') || ($this->request->server['HTTPS'] == '1'))) {
 			$connection = 'SSL';
@@ -21,15 +26,20 @@ class ControllerModuleLanguage extends Controller {
 			$connection = 'NONSSL';
 		}
 
+
 		$this->data['action'] = $this->url->link('module/language', '', $connection);
 
 		$this->data['language_code'] = $this->session->data['language'];
 
+
 		$this->load->model('localisation/language');
+
 
 		$this->data['languages'] = array();
 
+
 		$results = $this->model_localisation_language->getLanguages();
+
 
 		foreach ($results as $result) {
 			if ($result['status']) {
@@ -37,6 +47,7 @@ class ControllerModuleLanguage extends Controller {
 					'name'  => $result['name'],
 					'code'  => $result['code'],
 					'image' => $result['image']
+
 				);
 			}
 		}
@@ -49,16 +60,23 @@ class ControllerModuleLanguage extends Controller {
 		} else {
 			$data = $this->request->get;
 
+
 			unset($data['_route_']);
+
 
 			$route = $data['route'];
 
+
 			unset($data['route']);
+
 
 			$url = '';
 
+
 			if ($data) {
 				$url = '&' . urldecode(http_build_query($data, '', '&'));
+
+
 			}
 
 			$this->data['redirect'] = $this->url->link($route, $url, $connection);
@@ -67,6 +85,7 @@ class ControllerModuleLanguage extends Controller {
 				$this->data['languages'][$i]['redirect'] = $this->url->link($route, $url, $connection, $language['code']);
 			}
 		}
+
 
 		if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/language.tpl')) {
 			$this->template = $this->config->get('config_template') . '/template/module/language.tpl';
