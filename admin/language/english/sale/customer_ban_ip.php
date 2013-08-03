ab2eebf12b0306163804c98e9236ebb92d9f0947
Oleksandr Rebrov <webmaster@webplaneta.com.ua>
2013-08-03 20:19:31 +0300

    Merge language with opencart 1.5.5.1

--- admin/language/english/sale/customer_ban_ip.php
+++ admin/language/english/sale/customer_ban_ip.php
@@ -1,9 +1,9 @@
 <?php
 // Heading
-$_['heading_title']    = 'Customer IP Blacklist';
+$_['heading_title']    = 'Customer Ban IP';
 
 // Text
-$_['text_success']     = 'Success: You have modified customer IP blacklist!';
+$_['text_success']     = 'Success: You have modified customer ban IP!';
 
 // Column
 $_['column_ip']        = 'IP';
@@ -14,6 +14,6 @@ $_['column_action']    = 'Action';
 $_['entry_ip']         = 'IP:';
 
 // Error
-$_['error_permission'] = 'Warning: You do not have permission to modify customer IP blacklist!';
+$_['error_permission'] = 'Warning: You do not have permission to modify customer ban IP!';
 $_['error_ip']         = 'IP must be between 1 and 15 characters!';
 ?>
