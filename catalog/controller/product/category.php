7e837a4e9ba95d6ea903bb6164e20b56da6164db
ravilrrr <rec@ru63.ru>
2013-08-26 06:59:31 -0700

    добавил изображения для подкатегорий

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -203,7 +203,8 @@ class ControllerProductCategory extends Controller {
 				
 				$this->data['categories'][] = array(
 					'name'  => $result['name'] . ($this->config->get('config_product_count') ? ' (' . $product_total . ')' : ''),
-					'href'  => $this->url->link('product/category', 'path=' . $this->request->get['path'] . '_' . $result['category_id'] . $url)
+					'href'  => $this->url->link('product/category', 'path=' . $this->request->get['path'] . '_' . $result['category_id'] . $url),
+					'thumb' => $this->model_tool_image->resize(($result['image']=='' ? 'no_image.jpg' : $result['image']), $this->config->get('config_image_category_width'), $this->config->get('config_image_category_height'))
 				);
 			}
 			
