810267cc78f119cbef0326c1dcde9e44ee256c7c
Bogdan1975 <targus.bogdan@gmail.com>
2013-11-17 11:09:13 +0200

    Fix: deleting of '-' removed in cache image filenames

--- catalog/model/tool/image.php
+++ catalog/model/tool/image.php
@@ -59,7 +59,7 @@ class ModelToolImage extends Model {
 	 * Used to make filename without rusian letters, spaces, etc.
 	 */
 	public function slugify($string) {
-		return strtolower(trim(preg_replace('~[^0-9a-z\.]+~i', '-', html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8')), '-'));
+		return strtolower(trim(preg_replace('~[^0-9a-z\.]+~i', '-', html_entity_decode(preg_replace('~&([a-z]{1,2})(?:acute|cedil|circ|grave|lig|orn|ring|slash|th|tilde|uml);~i', '$1', htmlentities($string, ENT_QUOTES, 'UTF-8')), ENT_QUOTES, 'UTF-8'))));
 	}
 }
 ?>
