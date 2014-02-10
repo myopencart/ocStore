69dd58aca8400a4ff1001849389388ecb9b4191a
ravilrrr <rec@ru63.ru>
2014-02-10 15:31:43 +0300

    Fix модуля Information
    Fix Если у статьи порядок сортировки -1, то она не должна отображаться

--- catalog/controller/module/information.php
+++ catalog/controller/module/information.php
@@ -13,11 +13,13 @@ class ControllerModuleInformation extends Controller {
 		$this->data['informations'] = array();
 
 		foreach ($this->model_catalog_information->getInformations() as $result) {
-      		$this->data['informations'][] = array(
-        		'title' => $result['title'],
-	    		'href'  => $this->url->link('information/information', 'information_id=' . $result['information_id'])
-      		);
-    	}
+	      		if ($result['sort_order'] != '-1') {
+				$this->data['informations'][] = array(
+	        			'title' => $result['title'],
+		    			'href'  => $this->url->link('information/information', 'information_id=' . $result['information_id'])
+	      			);
+	    		}
+		}
 
 		$this->data['contact'] = $this->url->link('information/contact');
     	$this->data['sitemap'] = $this->url->link('information/sitemap');
