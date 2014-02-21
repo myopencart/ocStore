c9012f67ddc31fcda98bbce5dd571ed42c7d0033
ravilrrr <rec@ru63.ru>
2014-02-21 23:09:15 +0300

    Fix Правильная сортировка категорий в товарах

--- admin/model/catalog/category.php
+++ admin/model/catalog/category.php
@@ -217,7 +217,7 @@ class ModelCatalogCategory extends Model {
 			$sql .= " AND cd2.name LIKE '" . $this->db->escape($data['filter_name']) . "%'";
 		}
 
-		$sql .= " GROUP BY cp.category_id ORDER BY c.sort_order, name";
+		$sql .= " GROUP BY cp.category_id ORDER BY name, c.parent_id";
 		
 		if (isset($data['start']) || isset($data['limit'])) {
 			if ($data['start'] < 0) {
