d84395d452b574d89059a0c991c86c96f72af7b5
dinox-pro <webmaster@webplaneta.com.ua>
2015-08-01 00:58:12 +0300

    Add h1 and title to product

--- admin/language/english/catalog/product.php
+++ admin/language/english/catalog/product.php
@@ -28,6 +28,7 @@ $_['column_action']          = 'Action';
 $_['entry_name']             = 'Product Name';
 $_['entry_description']      = 'Description';
 $_['entry_meta_title'] 	     = 'Meta Tag Title';
+$_['entry_meta_h1']          = 'HTML Tag H1';
 $_['entry_meta_keyword'] 	 = 'Meta Tag Keywords';
 $_['entry_meta_description'] = 'Meta Tag Description';
 $_['entry_keyword']          = 'SEO Keyword';
