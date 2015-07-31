c10b96d92c260ba3e30c3524fdc0907d0b59a45c
dinox-pro <webmaster@webplaneta.com.ua>
2015-07-31 23:46:55 +0300

    Some fix, add logo, add rss links

--- admin/language/english/common/header.php
+++ admin/language/english/common/header.php
@@ -1,6 +1,6 @@
 <?php
 // Heading
-$_['heading_title']        = 'OpenCart';
+$_['heading_title']        = 'ocStore';
 
 // Text
 $_['text_order']           = 'Orders';
--- admin/language/russian/common/footer.php
+++ admin/language/russian/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer'] 	= '<a href="http://myopencart.com">ocStore</a> &copy; 2009-' . date('Y') . ' Все права защищены.';
+$_['text_footer'] 	= '<a target="_blank" href="http://myopencart.com">ocStore</a> &copy; 2009-' . date('Y') . ' Все права защищены.';
 $_['text_version'] 	= 'Версия ocStore %s';
