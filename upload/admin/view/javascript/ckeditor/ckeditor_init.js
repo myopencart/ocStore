637405ca15954d49dd77bd4f51f7ed74f359dc30
shoputils <support@shoputils.ru>
2015-11-15 03:24:14 +0300

    Add CKEditor

---
+++ upload/admin/view/javascript/ckeditor/ckeditor_init.js
@@ -0,0 +1,25 @@
+// CKEditor */
+function ckeditorInit(node, token) {
+  console.log('node: ' + node);
+  console.log('token: ' + token);
+  CKEDITOR.replace(node);
+  CKEDITOR.on('dialogDefinition', function (ev) {
+    for (i = 0; i < ev.data.definition.contents.length; i++) {
+      var button = ev.data.definition.contents[i].get('browse');
+      if (button !== null) {
+        button.hidden = false;
+        button.onClick = function() {
+          $('#modal-image').remove();
+          $.ajax({
+            url: 'index.php?route=common/filemanager&cke=' + this.filebrowser.target + '&token=' + token,
+            dataType: 'html',
+            success: function(html) {
+              $('body').append('<div class="modal ckeditor" id="modal-image">' + html + '</div>');
+              $('#modal-image').modal('show');
+            }
+          });
+        }
+      }
+    }
+  });
+}
