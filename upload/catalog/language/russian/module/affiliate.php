f717097e1180f2dc30773b0ce780a37d32d83bde
shoputils <support@shoputils.ru>
2016-03-30 01:35:07 +0300

    Fix translate

--- upload/catalog/language/russian/module/affiliate.php
+++ upload/catalog/language/russian/module/affiliate.php
@@ -3,8 +3,8 @@
 $_['heading_title']    = 'Кабинета Партнера';
 
 // Text
-$_['text_register']    = 'Авторизация';
-$_['text_login']       = 'Регистрация';
+$_['text_register']    = 'Регистрация';
+$_['text_login']       = 'Авторизация';
 $_['text_logout']      = 'Выйти';
 $_['text_forgotten']   = 'Напомнить пароль';
 $_['text_account']     = 'Личный кабинет';
