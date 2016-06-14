aebbe14b910c2d0cbc8257804a8a444905bad299
shoputils <support@shoputils.ru>
2016-06-14 14:12:32 +0300

    Fix 'Erron Could not load model catalog/model/total/{total}.php'
    Если физически удалить модуль учета в заказе - при попытке сменить
    статус заказа возникает ошибка 'Error: Could not load model
    catalog/model/total/{total}.php' из-за отсутствия проверки на
    существование файла модели модуля.

--- upload/catalog/model/checkout/order.php
+++ upload/catalog/model/checkout/order.php
@@ -363,7 +363,9 @@ class ModelCheckoutOrder extends Model {
 				$order_total_query = $this->db->query("SELECT * FROM `" . DB_PREFIX . "order_total` WHERE order_id = '" . (int)$order_id . "' ORDER BY sort_order ASC");
 
 				foreach ($order_total_query->rows as $order_total) {
-					$this->load->model('total/' . $order_total['code']);
+					if (is_file(DIR_APPLICATION . 'model/total/' . $order_info['code'] . '.php')) {
+						$this->load->model('total/' . $order_total['code']);
+					}
 
 					if (method_exists($this->{'model_total_' . $order_total['code']}, 'unconfirm')) {
 						$this->{'model_total_' . $order_total['code']}->unconfirm($order_id);
