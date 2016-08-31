12078a24077f869361d47d39c1919bd71ec468b2
dinox-pro <webmaster@webplaneta.com.ua>
2016-08-31 20:14:14 +0300

    Fix russian localisation

---
+++ upload/install/language/ru-ru/common/column_left.php
@@ -0,0 +1,7 @@
+<?php
+$_['text_license']       = 'Лицензионное соглашение';
+$_['text_installation']  = 'Параметры сервера';
+$_['text_configuration'] = 'Настройка доступа';
+$_['text_upgrade']       = 'Обновление';
+$_['text_finished']      = 'Установка завершена';
+$_['text_language']      = 'Выберите язык';
---
+++ upload/install/language/ru-ru/install/step_2.php
@@ -0,0 +1,59 @@
+<?php
+// Heading
+$_['heading_title']          = 'Параметры сервера';
+
+// Text
+$_['text_step_2']            = 'Проверьте что Ваш сервер настроен правильно';
+$_['text_install_php']       = '1. Убедитесь что настройки PHP соответствуют требованиям, указанным ниже.';
+$_['text_install_extension'] = '2. Убедитесь, что на сервере устанавлены перечисленные ниже библиотеки.';
+$_['text_install_db']        = '3. Убедитесь, что у Вас есть по крайней мере один доступный драйвер базы данных.';
+$_['text_install_file']      = '4. Убедитесь что Вы переименовали файлы указанные ниже.';
+$_['text_install_directory'] = '5. Убедитесь, что Вы установили правильные разрешения для перечисленных каталогов.';
+$_['text_current']           = 'Текущие Настройки';
+$_['text_required']          = 'Необходимые настройки';
+$_['text_extension']         = 'Дополнительные библиотеки';
+$_['text_db']            	 = 'База данных';
+$_['text_db_driver']         = 'Драйвер БД';
+$_['text_file']              = 'Файлы';
+$_['text_directory']         = 'Каталоги';
+$_['text_status']            = 'Статус';
+$_['text_version']           = 'Версия PHP';
+$_['text_global']            = 'Register Globals';
+$_['text_magic']             = 'Magic Quotes GPC';
+$_['text_file_upload']       = 'Загрузка файлов';
+$_['text_session']           = 'Session Auto Start';
+$_['text_gd']                = 'GD';
+$_['text_curl']              = 'cURL';
+$_['text_mcrypt']            = 'mCrypt';
+$_['text_zlib']              = 'ZLIB';
+$_['text_zip']               = 'ZIP';
+$_['text_mbstring']          = 'mbstring';
+$_['text_on']                = 'On';
+$_['text_off']               = 'Off';
+$_['text_writable']          = 'Доступен для записи';
+$_['text_unwritable']        = 'Недоступен для записи';
+$_['text_missing']           = 'Отсутствует';
+
+// Error
+$_['error_version']          = 'Warning: You need to use PHP 5.4 or above for OpenCart to work!';
+$_['error_file_upload']      = 'Warning: file_uploads needs to be enabled!';
+$_['error_session']          = 'Warning: OpenCart will not work with session.auto_start enabled!';
+$_['error_db']               = 'Warning: A database extension needs to be loaded in the php.ini for OpenCart to work!';
+$_['error_gd']               = 'Warning: GD extension needs to be loaded for OpenCart to work!';
+$_['error_curl']             = 'Warning: CURL extension needs to be loaded for OpenCart to work!';
+$_['error_mcrypt']           = 'Warning: mCrypt extension needs to be loaded for OpenCart to work!';
+$_['error_zlib']             = 'Warning: ZLIB extension needs to be loaded for OpenCart to work!';
+$_['error_zip']              = 'Warning: ZIP extension needs to be loaded for OpenCart to work!';
+$_['error_mbstring']         = 'Warning: mbstring extension needs to be loaded for OpenCart to work!';
+$_['error_catalog_exist']    = 'Warning: config.php does not exist. You need to rename config-dist.php to config.php!';
+$_['error_catalog_writable'] = 'Warning: config.php needs to be writable for OpenCart to be installed!';
+$_['error_admin_exist']      = 'Warning: admin/config.php does not exist. You need to rename admin/config-dist.php to admin/config.php!';
+$_['error_admin_writable']   = 'Warning: admin/config.php needs to be writable for OpenCart to be installed!';
+$_['error_image']            = 'Warning: Image directory needs to be writable for OpenCart to work!';
+$_['error_image_cache']      = 'Warning: Image cache directory needs to be writable for OpenCart to work!';
+$_['error_image_catalog']    = 'Warning: Image catalog directory needs to be writable for OpenCart to work!';
+$_['error_cache']            = 'Warning: Cache directory needs to be writable for OpenCart to work!';
+$_['error_log']              = 'Warning: Logs directory needs to be writable for OpenCart to work!';
+$_['error_download']         = 'Warning: Download directory needs to be writable for OpenCart to work!';
+$_['error_upload']           = 'Warning: Upload directory needs to be writable for OpenCart to work!';
+$_['error_modification']     = 'Warning: Modification directory needs to be writable for OpenCart to work!';
