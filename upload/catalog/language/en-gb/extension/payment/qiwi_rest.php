11f67d60288d3c485379d678df32d73ec0165c54
dinox-pro <webmaster@webplaneta.com.ua>
2016-09-01 21:55:15 +0300

    Fixed russian language in catalog

---
+++ upload/catalog/language/en-gb/extension/payment/qiwi_rest.php
@@ -0,0 +1,36 @@
+<?php
+// Text
+$_['text_title'] 			= 'QIWI Wallet';
+$_['sub_text_info'] 		= '';
+$_['sub_text_info_phone'] 	= 'QIWI Wallet (Telephone number) : +';
+$_['markup'] 			    = 'Total in %s + markup(%s%%) : ';
+$_['order_id'] 			    = 'Order №';
+$_['qiwi_rest_limit'] 		= '<font color="red">Exceeded the limit on the maximum amount of the order in qiwi (15000 rubles)</font>';
+$_['button_back'] 		    = 'Shopping Cart';
+$_['qiwi_rest_rubls'] 		= ' rubles';
+$_['qiwi_pcs'] 			    = ' pcs ';
+
+$_['qiwi_error'] 			= 'Error';
+$_['qiwi_error_5'] 		    = 'There are no values ​​in the request parameters';
+$_['qiwi_error_13'] 		= 'The server is busy, try again later';
+$_['qiwi_error_78'] 		= 'Illegal Operation';
+$_['qiwi_error_150'] 		= 'Error authorization';
+$_['qiwi_error_152'] 		= 'Not connected or disconnected Protocol';
+$_['qiwi_error_210'] 		= 'Account not found';
+$_['qiwi_error_215'] 		= 'Account with bill_id already exists';
+$_['qiwi_error_220'] 		= 'Not enough funds in the account for payment';
+$_['qiwi_error_241'] 		= 'The amount is too small';
+$_['qiwi_error_242'] 		= 'The sum is too high';
+$_['qiwi_error_298']		= 'Purse with this number is not registered';
+$_['qiwi_error_300'] 		= 'Technical error';
+$_['qiwi_error_303'] 		= 'Wrong phone number';
+$_['qiwi_error_316'] 		= 'Attempt blocked authorization provider';
+$_['qiwi_error_319'] 		= 'There are no rights for this operation';
+$_['qiwi_error_341'] 		= 'Required parameter is incorrect or missing in the request';
+$_['qiwi_error_700'] 		= 'exceeded the monthly limit on operations';
+$_['qiwi_error_702'] 		= 'limit exceeded for the remainder of the subscribers account Visa QIWI Wallet';
+$_['qiwi_error_1001'] 		= 'Forbidden currency for provider';
+$_['qiwi_error_1003'] 		= 'Unable to get the conversion rate for the currency pair';
+$_['qiwi_error_1019'] 		= 'Unable to determine the cellular operator for mobile commerce';
+
+$_['qiwi_continue'] 		= 'Pay now';
