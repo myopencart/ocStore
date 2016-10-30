76b04962e7f3e0623255f3de988690a2983b54f6
shchs <shchs@ya.ru>
2016-10-30 11:02:36 +0300

    Update cart.php
    Чтобы проверить есть ли в корзине товары, не нужно использовать тяжёлую функцию getProducts, можно намного проще сделать.

--- upload/system/library/cart/cart.php
+++ upload/system/library/cart/cart.php
@@ -368,7 +368,8 @@ class Cart {
 	}
 
 	public function hasProducts() {
-		return count($this->getProducts());
+		$query = $this->db->query("SELECT SUM(quantity) as total FROM " . DB_PREFIX . "cart WHERE api_id = '" . (isset($this->session->data['api_id']) ? (int)$this->session->data['api_id'] : 0) . "' AND customer_id = '" . (int)$this->customer->getId() . "' AND session_id = '" . $this->db->escape($this->session->getId()) . "'");
+		return $query->row['total'];
 	}
 
 	public function hasRecurringProducts() {
