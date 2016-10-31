26ec4a6b71845a4fa2a9aac43ae2ab95124c86da
shoputils <support@shoputils.ru>
2016-10-31 10:27:10 +0300

    Fix option no_image

--- upload/catalog/controller/product/product.php
+++ upload/catalog/controller/product/product.php
@@ -361,7 +361,7 @@ class ControllerProductProduct extends Controller {
 							'product_option_value_id' => $option_value['product_option_value_id'],
 							'option_value_id'         => $option_value['option_value_id'],
 							'name'                    => $option_value['name'],
-							'image'                   => $this->model_tool_image->resize($option_value['image'], 50, 50),
+							'image'                   => $option_value['image'] ? $this->model_tool_image->resize($option_value['image'], 50, 50) : '',
 							'price'                   => $price,
 							'price_prefix'            => $option_value['price_prefix']
 						);
