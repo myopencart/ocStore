c642a8649f690d0ed776e78b7a52ac455a380c76
dinox-pro <webmaster@webplaneta.com.ua>
2016-11-20 23:45:30 +0200

    Fix https://opencartforum.com/topic/66829-ocstore-23-финальное-тестирование-перед-релизом/?p=616630

--- upload/admin/controller/user/user_permission.php
+++ upload/admin/controller/user/user_permission.php
@@ -357,8 +357,9 @@ class ControllerUserUserPermission extends Controller {
 
 		while (count($path) != 0) {
 			$next = array_shift($path);
-
-			foreach (glob($next) as $file) {
+            $g_files = glob($next);
+            if (!$g_files) $g_files = array();
+			foreach ($g_files as $file) {
 
 				// If directory add to path array
 				if (is_dir($file)) {
