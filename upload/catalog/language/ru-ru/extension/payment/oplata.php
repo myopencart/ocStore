e9fbf3955ddd289e8f000b608fedc0df0e698a72
dinox-pro <webmaster@webplaneta.com.ua>
2017-03-21 19:20:09 +0200

    Add Fondy payment

---
+++ upload/catalog/language/ru-ru/extension/payment/oplata.php
@@ -0,0 +1,9 @@
+<?php
+// Text
+$_['text_title'] = 'FONDY VISA/MasterCard';
+$_['error_merchant'] = 'An error has occurred during payment. Merchant data is incorrect.';
+$_['error_signature'] = 'An error has occurred during payment. Signature is not valid.';
+$_['error_transaction'] = 'Transaction has been declined.';
+$_['error_oplata'] = 'Оплата отклонена по причине :';
+$_['error_kod'] = 'Код ошибки :';
+?>
