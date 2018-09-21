e6269b246a1bea6a30ab97893a37f52f3dcf2032
dinox-pro <webmaster@webplaneta.com.ua>
2018-09-21 21:46:14 +0300

    Change ocstore site

--- upload/admin/language/en-gb/common/footer.php
+++ upload/admin/language/en-gb/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer'] 	= '<a target="_blank" href="http://myopencart.com/">ocStore</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
+$_['text_footer'] 	= '<a target="_blank" href="https://ocstore.com/">ocStore</a> &copy; 2009-' . date('Y') . ' All Rights Reserved.';
 $_['text_version'] 	= 'Version ocStore %s';
--- upload/catalog/language/en-gb/common/footer.php
+++ upload/catalog/language/en-gb/common/footer.php
@@ -14,4 +14,4 @@ $_['text_account']      = 'My Account';
 $_['text_order']        = 'Order History';
 $_['text_wishlist']     = 'Wish List';
 $_['text_newsletter']   = 'Newsletter';
-$_['text_powered']      = 'Powered By <a target="_blank" href="http://myopencart.com/">ocStore</a><br /> %s &copy; %s';
+$_['text_powered']      = 'Powered By <a target="_blank" href="https://ocstore.com/">ocStore</a><br /> %s &copy; %s';
