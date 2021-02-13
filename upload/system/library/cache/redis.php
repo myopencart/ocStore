00a1a8419e8614a27bf76ca26091977a89ff0df9
Bevar-Pro <79024249+Bevar-Pro@users.noreply.github.com>
2021-02-14 01:11:54 +0200

    Add files via upload

---
+++ upload/system/library/cache/redis.php
@@ -0,0 +1,42 @@
+<?php
+namespace Cache;
+class Redis {
+	private $expire;
+	private $cache;
+
+	public function __construct($expire) {
+		
+		$this->expire = $expire;
+
+		$this->cache = new \Redis();
+		$this->cache->pconnect(CACHE_HOSTNAME, CACHE_PORT);
+	}
+
+	public function get($key) {
+		
+		if ($this->cache->exists($key)) {
+			$data = $this->cache->get(CACHE_PREFIX . $key);
+			return json_decode($data, true);
+		}
+		
+		return false;
+	}
+
+	public function set($key, $value, $expire = '') {
+		if (!$expire) {
+			$expire = $this->expire;
+		}
+
+		$status = $this->cache->set(CACHE_PREFIX . $key, json_encode($value));
+
+		if ($status) {
+			$this->cache->expire(CACHE_PREFIX . $key, $expire);
+		}
+
+		return $status;
+	}
+
+	public function delete($key) {
+		$this->cache->del(CACHE_PREFIX . $key);
+	}
+}
