9255f32efcd28ca2f99a48ca794f44a753091543
dinox-pro <rebrovoleksandr@gmail.com>
2021-03-03 14:47:47 +0200

    Fix DB_PREFIX for install

--- upload/install/controller/install/step_3.php
+++ upload/install/controller/install/step_3.php
@@ -37,7 +37,7 @@ class ControllerInstallStep3 extends Controller {
 			$output .= 'define(\'DB_PASSWORD\', \'' . addslashes(html_entity_decode($this->request->post['db_password'], ENT_QUOTES, 'UTF-8')) . '\');' . "\n";
 			$output .= 'define(\'DB_DATABASE\', \'' . addslashes($this->request->post['db_database']) . '\');' . "\n";
 			$output .= 'define(\'DB_PORT\', \'' . addslashes($this->request->post['db_port']) . '\');' . "\n";
-			$output .= 'define(\'DB_PORT\', \'' . addslashes($options['db_port']) . '\');' . "\n\n";
+            $output .= 'define(\'DB_PREFIX\', \'' . addslashes($this->request->post['db_prefix']) . '\');' . "\n\n";
 
 			$output .= '// Redis' . "\n";
 			$output .= '// define(\'CACHE_HOSTNAME\', \'localhost\');' . "\n";
