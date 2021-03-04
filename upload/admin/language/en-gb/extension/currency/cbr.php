77532c17bc4afe4d03487f4f63c35bfc4e85a16f
dinox-pro <rebrovoleksandr@gmail.com>
2021-03-04 14:32:36 +0200

    Added bank servers  for currency converting

---
+++ upload/admin/language/en-gb/extension/currency/cbr.php
@@ -0,0 +1,15 @@
+<?php
+// Heading
+$_['heading_title']    = 'Central Bank of the Russian Federation';
+
+// Text
+$_['text_extension']   = 'Extensions';
+$_['text_success']     = 'Success: You have modified fixer CBR rates!';
+$_['text_edit']        = 'Edit European Central Bank';
+$_['text_support']     = 'This extension requires at RUB currency to be available currency option.';
+
+// Entry
+$_['entry_status']     = 'Status';
+
+// Error
+$_['error_permission'] = 'Warning: You do not have permission to modify CBR currency rates!';
