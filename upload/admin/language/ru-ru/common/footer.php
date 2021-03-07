45d2badce7a217059013884416b2e92c9d10775e
dinox-pro <rebrovoleksandr@gmail.com>
2021-03-07 17:22:59 +0200

    Add utm for links

--- upload/admin/language/ru-ru/common/footer.php
+++ upload/admin/language/ru-ru/common/footer.php
@@ -1,4 +1,4 @@
 <?php
 // Text
-$_['text_footer'] 	= '<a target="_blank" href="http://ocstore.com">ocStore</a> &copy; 2009-' . date('Y') . ' Все права защищены.';
-$_['text_version'] 	= 'Версия ocStore %s';
+$_['text_footer'] 	= '<a target="_blank" href="http://ocstore.com/?utm_source=ocstore23">ocStore</a> &copy; 2009-' . date('Y') . ' Все права защищены.';
+$_['text_version'] 	= 'Версия ocStore %s<br/>Максимальная производительность <a target="_blank" href="https://turbohost.pro/?utm_source=ocstore23">TurboHost.pro</a>';
