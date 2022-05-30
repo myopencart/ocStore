956c9d5bd533b492b062a5cce1d42b21e60036e0
BuslikDrev <32706609+BuslikDrev@users.noreply.github.com>
2022-05-30 08:22:14 +0200

    Update native.php

--- upload/system/library/session/native.php
+++ upload/system/library/session/native.php
@@ -1,38 +1,38 @@
 <?php
 namespace Session;
 class Native extends \SessionHandler {
-    #[\ReturnTypeWillChange]
-    public function create_sid() {
-        return parent::create_sid();
-    }
+	#[\ReturnTypeWillChange]
+	public function create_sid() {
+		return parent::create_sid();
+	}
 
-    #[\ReturnTypeWillChange]
-    public function open($path, $name) {
-        return parent::open($path, $name);
-    }
+	#[\ReturnTypeWillChange]
+	public function open($path, $name) {
+		return parent::open($path, $name);
+	}
 
-    #[\ReturnTypeWillChange]
-    public function close() {
-        return parent::close();
-    }
+	#[\ReturnTypeWillChange]
+	public function close() {
+		return parent::close();
+	}
 
-    #[\ReturnTypeWillChange]
-    public function read($session_id) {
-        return parent::read($session_id);
-    }
+	#[\ReturnTypeWillChange]
+	public function read($session_id) {
+		return parent::read($session_id);
+	}
 
-    #[\ReturnTypeWillChange]
-    public function write($session_id, $data) {
+	#[\ReturnTypeWillChange]
+	public function write($session_id, $data) {
 		return parent::write($session_id, $data);
-    }
+	}
 
-    #[\ReturnTypeWillChange]
-    public function destroy($session_id) {
-        return parent::destroy($session_id);
-    }
+	#[\ReturnTypeWillChange]
+	public function destroy($session_id) {
+		return parent::destroy($session_id);
+	}
 
-    #[\ReturnTypeWillChange]
-    public function gc($maxlifetime) {
-        return parent::gc($maxlifetime);
-    }	
+	#[\ReturnTypeWillChange]
+	public function gc($maxlifetime) {
+		return parent::gc($maxlifetime);
+	}	
 }
